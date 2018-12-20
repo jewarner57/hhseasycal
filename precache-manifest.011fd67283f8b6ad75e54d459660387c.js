a4e72ce998d7534f7d373fdb158d64b4e1a5fede
Jonathan Warner <jonathanwarner@jonathans-mbp.lan>
2018-12-20 13:36:04 -0500

    12/20/18  - Fixed issue where only /static would commit

---
+++ precache-manifest.011fd67283f8b6ad75e54d459660387c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5864cc05c8e3111a82f",
+    "url": "/static/js/main.d5864cc0.chunk.js"
+  },
+  {
+    "revision": "074fd19af1f912b809c3",
+    "url": "/static/js/1.074fd19a.chunk.js"
+  },
+  {
+    "revision": "d5864cc05c8e3111a82f",
+    "url": "/static/css/main.4eb658d4.chunk.css"
+  },
+  {
+    "revision": "19ea3523674d68ce507c1911244f5d95",
+    "url": "/index.html"
+  }
+];
